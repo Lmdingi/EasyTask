c10774e2061fbcb9a82d92b4383e85fcbdb24d37
Lmdingi <lmdingi4@gmail.com>
2024-11-14 18:09:23 +0200

    add easy task

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+// For Standalone
+/**************** */
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { AppComponent } from './app/app.component';
+
+// bootstrapApplication(AppComponent).catch((err) => console.error(err));
+/**********************************************************************/
+
+// For Module
+/**************** */
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
+/**********************************************************************/
